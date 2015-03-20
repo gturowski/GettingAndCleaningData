aff8ae3566aa13a9457dcb95a71530ee50366991
Grzegorz Turowski <grzetur@gmail.com>
2015-03-19 21:41:55 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -30,8 +30,7 @@ DS<-merge(testDS2, trainDS2, all=TRUE, sort=FALSE)
 features<- read.table("UCI HAR Dataset/features.txt")
 select_features<-features[((grepl("mean()", features$V2))&!(grepl("meanFreq()", features$V2)))|((grepl("std()", features$V2))),]
 feature_hlp<-select_features[,1]+2
-V1<-feature_hlp   
-final_data<-DS[,V1]      #Display only selected Variables
+final_data<-DS[,feature_hlp]      #Display only selected Variables
 final_data<-cbind(activity=DS$activity,final_data)
 final_data<-cbind(subject=DS$subject,final_data)
 c_names<-as.vector(select_features[,2])
